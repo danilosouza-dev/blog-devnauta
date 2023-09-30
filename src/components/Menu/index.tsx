62308e4e2bffc47733fdf5e3200bce2d354d1d65
Danilo Souza <danilo@reload.co>
2023-09-30 13:06:12 -0300

    feat: create component Card

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,5 +1,9 @@
 import styles from './menu.module.css'
 
 export function Menu() {
-  return <div className={styles.container}>Menu</div>
+  return (
+    <div className={styles.container}>
+      <h2>What's hot?</h2>
+    </div>
+  )
 }
--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -6,27 +6,52 @@ export function Card() {
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src="/p1.jpeg" alt="image post" fill className={styles.image} />
+        <Image
+          src="/p1.jpeg"
+          alt="image post"
+          // width={295}
+          // height={287}
+          fill
+          className={styles.image}
+        />
       </div>
 
       <div className={styles.textContainer}>
         <div className={styles.detail}>
-          <span className={styles.date}>11.08.2023 - </span>
-          <span className={styles.category}>CULTURE</span>
+          {/* <span className={styles.date}>11.08.2023 - </span> */}
+          <span className={styles.categoryTag}>culture</span>
         </div>
-        <Link href="/">
+        <Link href="/" className={styles.linkTitle}>
           <h1 className={styles.title}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit
           </h1>
         </Link>
-        <p className={styles.description}>
+        {/* <p className={styles.description}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
           consectetur delectus nemo quaerat aliquam! Quos similique atque
           laboriosam numquam.
-        </p>
-        <Link href="/" className={styles.link}>
+        </p> */}
+
+        <div className={styles.profileContainer}>
+          <div className={styles.imageProfileContainer}>
+            <Image
+              src="/fashion.png"
+              alt="profile"
+              fill
+              className={styles.imgProfile}
+            />
+          </div>
+          <div className={styles.infoProfile}>
+            <h6 className={styles.name}>Danilo Souza</h6>
+            <div className={styles.dateContainer}>
+              <span className={styles.dateProfile}>24 de janeiro, 2023 </span>
+              {/* <span className={styles.timeToRead}> 4 minutos de leitura</span> */}
+            </div>
+          </div>
+        </div>
+        {/* <Link href="/" className={styles.link}>
           Read More
-        </Link>
+        </Link> */}
       </div>
     </div>
   )
